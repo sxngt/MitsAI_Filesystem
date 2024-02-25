f0186087881df2a80121020f3d172fcb8c6b8014
sxngt <sxngt@icloud.com>
2024-02-25 22:49:14 +0900

    add folder data name -> list func

--- dataloader.py
+++ dataloader.py
@@ -1,16 +1,23 @@
+import os
+
 from database.conn import mongo
 from database.config import URI
 
 mongo.init_app(uri=URI, port=27017)
 
-#싱글턴처럼 사용합시다...
+# 싱글턴처럼 사용합시다...
 
 mongo.connect("chest_image")
 
-#with open('local_data/ID00426637202313170790466_91.jpg', 'rb') as f:
+# with open('local_data/ID00426637202313170790466_91.jpg', 'rb') as f:
 #    res = mongo.gfs_upload(f, filename="chest1.jpg")
-#print(res)
+# print(res)
 local_download_path = "/Users/ysh/Dev/Python/mitsAI/download/"
 filename = "chest1.jpg"
-mongo.gfs_download(output_file_path=local_download_path+filename, filename=filename)
+mongo.gfs_download(output_file_path=local_download_path + filename, filename=filename)
 mongo.close()
+
+
+def data_name_to_list(folder_path: str) -> list:
+    file_and_dirs = os.listdir(folder_path)
+    return file_and_dirs
